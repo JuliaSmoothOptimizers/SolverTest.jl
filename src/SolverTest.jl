ba5a6c659ce7d1457924b80635cb0f434063d22e
Abel <abel.s.siqueira@gmail.com>
2021-03-24 22:18:10 -0300

    Add NLS multiprecision test

--- src/SolverTest.jl
+++ src/SolverTest.jl
@@ -5,10 +5,12 @@ using LinearAlgebra, Logging, SparseArrays, Test
 # JSO
 using ADNLPModels, NLPModels
 
-include("multiprecision.jl")
 include("nlp/unconstrained.jl")
 include("nlp/bound-constrained.jl")
+include("nlp/multiprecision.jl")
+
 include("nls/unconstrained.jl")
 include("nls/bound-constrained.jl")
+include("nls/multiprecision.jl")
 
 end # module
--- src/nls/multiprecision.jl
+++ src/nls/multiprecision.jl
@@ -1,7 +1,7 @@
-export multiprecision_nlp
+export multiprecision_nls
 
 """
-    multiprecision_nlp(solver, problem_type; precisions=[Float16, …, BigFloat])
+    multiprecision_nls(solver, problem_type; precisions=[Float16, …, BigFloat])
 
 Test that `solver` solves a problem of type `problem_type` on various `precisions`.
 The `problem_type` can be
@@ -12,34 +12,34 @@ The `problem_type` can be
 - :eqnbnd
 - :gen
 """
-function multiprecision_nlp(solver, ptype)
-  f(x) = (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2
+function multiprecision_nls(solver, ptype)
+  F(x) = [x[1] - 1; 2 * (x[2] - x[1]^2)]
   c(x) = [x[1]^2 + x[2]^2]
   c2(x) = [c(x); x[2] - x[1]^2 / 10]
   @testset "Precision $T for ptype $ptype" for T in (Float16, Float32, Float64, BigFloat)
     x0 = T[-1.2; 1.0]
     ℓ, u = zeros(T, 2), 2 * ones(T, 2)
-    nlp = if ptype == :unc
-      ADNLPModel(f, x0)
+    nls = if ptype == :unc
+      ADNLSModel(F, x0, 2)
     elseif ptype == :bnd
-      ADNLPModel(f, x0, ℓ, u)
+      ADNLSModel(F, x0, 2, ℓ, u)
     elseif ptype == :equ
-      ADNLPModel(f, x0, c, [2.0], [2.0])
+      ADNLSModel(F, x0, 2, c, [2.0], [2.0])
     elseif ptype == :ineq
-      ADNLPModel(f, x0, c, [-1.0], [1.0])
+      ADNLSModel(F, x0, 2, c, [-1.0], [1.0])
     elseif ptype == :eqnbnd
-      ADNLPModel(f, x0, ℓ, u, c, [2.0], [2.0])
+      ADNLSModel(F, x0, 2, ℓ, u, c, [2.0], [2.0])
     elseif ptype == :gen
-      ADNLPModel(f, x0, ℓ, u, c2, [2.0; 0.0], [2.0; Inf])
+      ADNLSModel(F, x0, 2, ℓ, u, c2, [2.0; 0.0], [2.0; Inf])
     else
       error("Unexpected ptype $ptype")
     end
     ϵ = eps(T)^T(1/4)
 
-    ng0 = norm(grad(nlp, nlp.meta.x0))
+    ng0 = norm(grad(nls, nls.meta.x0))
 
     stats = with_logger(NullLogger()) do
-      solver(nlp, atol=ϵ, rtol=ϵ)
+      solver(nls, atol=ϵ, rtol=ϵ)
     end
     @test eltype(stats.solution) == T
     @test stats.objective isa T
