c5d297a864ec4afc32275cd5a96624baad4ceccc
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-03-29 12:15:40 -0300

    Create docs

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter, SolverTest
+
+makedocs(
+  modules = [SolverTest],
+  doctest = true,
+  linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",
+                           assets = ["assets/style.css"]),
+  sitename = "SolverTest.jl",
+  pages = [
+    "Home" => "index.md"
+  ]
+)
+
+deploydocs(
+  repo = "github.com/JuliaSmoothOptimizers/SolverTest.jl.git",
+  devbranch = "main",
+  push_preview = true
+)
