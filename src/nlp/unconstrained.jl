2ee7e9369db256a913aa30cd7dff9511dbbba320
tmigot <tangi.migot@gmail.com>
2021-04-02 19:27:33 +0200

    split tests and add tolerances

--- src/nlp/unconstrained.jl
+++ src/nlp/unconstrained.jl
@@ -1,15 +1,10 @@
 export unconstrained_nlp
 
-"""
-    unconstrained_nlp(solver)
-
-Test the `solver` on unconstrained problems.
-"""
-function unconstrained_nlp(solver)
+function unconstrained_set()
   n = 30
   D = Diagonal([0.1 + 0.9 * (i - 1) / (n - 1) for i = 1:n])
   A = spdiagm(0 => 2 * ones(n), -1 => -ones(n-1), -1 => -ones(n-1))
-  @testset "Problem $(nlp.meta.name)" for nlp in [
+  return [
     ADNLPModel(
       x -> (x[1] - 1)^2 + 4 * (x[2] - 1)^2,
       zeros(2),
@@ -36,13 +31,23 @@ function unconstrained_nlp(solver)
       name = "Extended Rosenbrock"
     ),
   ]
+end
+
+"""
+    unconstrained_nlp(solver; atol=1e-6, rtol=1e-6)
+
+Test the `solver` on unconstrained problems.
+If `rtol` is non-zero, the relative error uses the gradient at the initial guess.
+"""
+function unconstrained_nlp(solver; atol=1e-6, rtol=1e-6)
+  @testset "Problem $(nlp.meta.name)" for nlp in unconstrained_set()
     stats = with_logger(NullLogger()) do
       solver(nlp)
     end
-    ng0 = norm(grad(nlp, nlp.meta.x0))
-    @test isapprox(stats.solution, ones(nlp.meta.nvar), atol=1e-6 * (ng0 + 1))
-    @test isapprox(stats.objective, 0.0, atol=1e-6 * (ng0 + 1))
-    @test stats.dual_feas < 1e-6 * (ng0 + 1)
+    ng0 = rtol != 0 ? norm(grad(nlp, nlp.meta.x0)) : 0
+    @test isapprox(stats.solution, ones(nlp.meta.nvar), atol=atol+rtol*ng0)
+    @test isapprox(stats.objective, 0.0, atol=atol+rtol*ng0)
+    @test stats.dual_feas < atol+rtol*ng0
     @test stats.status == :first_order
   end
 end
