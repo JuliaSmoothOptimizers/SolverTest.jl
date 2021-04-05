917036f72b4424a6909976aa66d993411f184c35
tmigot <tangi.migot@gmail.com>
2021-04-05 17:17:58 +0200

    split and add tol: bound-cons

--- src/nlp/unconstrained.jl
+++ src/nlp/unconstrained.jl
@@ -34,7 +34,7 @@ function unconstrained_set()
 end
 
 """
-    unconstrained_nlp(solver; atol=1e-6, rtol=1e-6)
+    unconstrained_nlp(solver; problem_set = unconstrained_set(), atol = 1e-6, rtol = 1e-6)
 
 Test the `solver` on unconstrained problems.
 If `rtol` is non-zero, the relative error uses the gradient at the initial guess.
