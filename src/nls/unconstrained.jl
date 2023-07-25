e17135e966d3cb54ce075060970472af6f74b693
tmigot <tangi.migot@gmail.com>
2023-07-25 14:19:20 -0400

    Add kwargs in unconstrained NLS

--- src/nls/unconstrained.jl
+++ src/nls/unconstrained.jl
@@ -1,30 +1,33 @@
 export unconstrained_nls
 
-function unconstrained_nls_set()
+function unconstrained_nls_set(;kwargs...)
   n = 30
   D = Diagonal([0.1 + 0.9 * (i - 1) / (n - 1) for i = 1:n])
   A = spdiagm(0 => 2 * ones(n), -1 => -ones(n - 1), -1 => -ones(n - 1))
   return [
-    ADNLSModel(x -> [x[1] - 1; 2x[2] - 2], zeros(2), 2, name = "(x₁ - 1)² + 4(x₂ - 1)²"),
-    ADNLSModel(x -> sqrt.(D) * (x .- 1), zeros(n), n, name = "Diagonal quadratic"),
+    ADNLSModel(x -> [x[1] - 1; 2x[2] - 2], zeros(2), 2, name = "(x₁ - 1)² + 4(x₂ - 1)²";kwargs...),
+    ADNLSModel(x -> sqrt.(D) * (x .- 1), zeros(n), n, name = "Diagonal quadratic"; kwargs...),
     ADNLSModel(
       x -> [x[1]^2 + x[2]^2 - 1; x[1] + x[2] - 4],
       zeros(2),
       2,
-      name = "Non-zero residual",
+      name = "Non-zero residual";
+      kwargs...,
     ),
-    ADNLSModel(x -> [x[1] - 1; 10 * (x[2] - x[1]^2)], [-1.2; 1.0], 2, name = "Rosenbrock"),
+    ADNLSModel(x -> [x[1] - 1; 10 * (x[2] - x[1]^2)], [-1.2; 1.0], 2, name = "Rosenbrock"; kwargs...),
     ADNLSModel(
       x -> [x[1] - 1; 10 * (x[2] - x[1]^2); x[1] * x[2] - 1],
       [-1.2; 1.0],
       3,
-      name = "Overdetermined",
+      name = "Overdetermined";
+      kwargs...,
     ),
     ADNLSModel(
       x -> [[10 * (x[i + 1] - x[i]^2) for i = 1:(n - 1)]; [x[i] - 1 for i = 1:(n - 1)]],
       collect(1:n) ./ (n + 1),
       2n - 2,
-      name = "Extended Rosenbrock",
+      name = "Extended Rosenbrock";
+      kwargs...,
     ),
   ]
 end
