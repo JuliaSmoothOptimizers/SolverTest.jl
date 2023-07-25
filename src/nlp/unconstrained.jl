13f6b7bc00722ac5127577083b68c0e3d53f7f2d
d-monnet <70266099+d-monnet@users.noreply.github.com>
2023-07-25 14:09:38 -0400

    add gradient backend for multi prec compat (#52)
    * Update unconstrained.jl
    ---------
    
    Co-authored-by: tmigot <tangi.migot@gmail.com>

--- src/nlp/unconstrained.jl
+++ src/nlp/unconstrained.jl
@@ -1,18 +1,19 @@
 export unconstrained_nlp
 
-function unconstrained_nlp_set()
+function unconstrained_nlp_set(;kwargs...)
   n = 30
   D = Diagonal([0.1 + 0.9 * (i - 1) / (n - 1) for i = 1:n])
   A = spdiagm(0 => 2 * ones(n), -1 => -ones(n - 1), -1 => -ones(n - 1))
   return [
-    ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 1)^2, zeros(2), name = "(x₁ - 1)² + 4(x₂ - 1)²"),
-    ADNLPModel(x -> dot(x .- 1, D, x .- 1), zeros(n), name = "Diagonal quadratic"),
-    ADNLPModel(x -> dot(x .- 1, A, x .- 1), zeros(n), name = "Tridiagonal quadratic"),
-    ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0], name = "Rosenbrock"),
+    ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 1)^2, zeros(2), name = "(x₁ - 1)² + 4(x₂ - 1)²"; kwargs...),
+    ADNLPModel(x -> dot(x .- 1, D, x .- 1), zeros(n), name = "Diagonal quadratic"; kwargs...),
+    ADNLPModel(x -> dot(x .- 1, A, x .- 1), zeros(n), name = "Tridiagonal quadratic"; kwargs...),
+    ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0], name = "Rosenbrock"; kwargs...),
     ADNLPModel(
       x -> sum(100 * (x[i + 1] - x[i]^2)^2 + (x[i] - 1)^2 for i = 1:(n - 1)),
       collect(1:n) ./ (n + 1),
-      name = "Extended Rosenbrock",
+      name = "Extended Rosenbrock";
+      kwargs...,
     ),
   ]
 end
