a67047cbddadc628c145d37b074846dfcf291152
Tangi Migot <tangi.migot@gmail.com>
2026-03-07 12:10:06 -0500

    Remove prettyurls configuration from make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ makedocs(
   linkcheck = true,
   strict = true,
   format = Documenter.HTML(
-    prettyurls = get(ENV, "CI", nothing) == "true",
     assets = ["assets/style.css"],
   ),
   sitename = "SolverTest.jl",
